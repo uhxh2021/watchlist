40125368527e8c77b6730cde470b8ba25d89eee0
uhxh2021 <uhxh2021@163.com>
2021-09-24 11:58:14 +0800

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -15,9 +15,9 @@ else:
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path. \
-    join(os.path.dirname(app.root_path), 'data.db')
+    join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 db = SQLAlchemy(app)
 
 login_manager = LoginManager(app)
